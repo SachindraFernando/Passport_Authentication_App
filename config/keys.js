5ad0863be97007d1d4aa7be0f41a2411eb4bf676
Sachindra Fernando <sachindrafernando3@gmail.com>
2020-03-14 23:36:11 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://sachindrafernando3:<123>@cluster0-yyiem.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://sachi3:123456@cluster0-yyiem.mongodb.net/test?retryWrites=true&w=majority';
 module.exports = {
     mongoURI: dbPassword
 };
